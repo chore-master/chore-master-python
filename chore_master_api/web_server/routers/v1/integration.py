887b173f55d66f6c393a44cb832cbc318a2e45da
gocreating <gocreating@gmail.com>
2024-06-03 00:12:46 +0800

    Update

--- chore_master_api/web_server/routers/v1/integration.py
+++ chore_master_api/web_server/routers/v1/integration.py
@@ -28,7 +28,11 @@ router = APIRouter(prefix="/integrations", tags=["Integration"])
 
 
 def create_spreadsheet_if_not_exist(
-    parent_folder_id: str, spreadsheet_name: str, drive_service: Resource
+    parent_folder_id: str,
+    spreadsheet_name: str,
+    sheet_titles: list[str],
+    drive_service: Resource,
+    sheets_service: Resource,
 ) -> str:
     spreadsheet_mime_type = "application/vnd.google-apps.spreadsheet"
     result = (
@@ -46,12 +50,45 @@ def create_spreadsheet_if_not_exist(
             "mimeType": spreadsheet_mime_type,
             "parents": [parent_folder_id],
         }
-        file = drive_service.files().create(body=file_metadata, fields="id").execute()
-        return file["id"]
+        file = (
+            drive_service.files()
+            .create(body=file_metadata, fields="id, name")
+            .execute()
+        )
+        spreadsheet_id = file["id"]
     elif len(files) == 1:
-        return files[0]["id"]
+        spreadsheet_id = files[0]["id"]
     elif len(files) > 1:
         raise Exception(f"Multiple spreadsheets named `{spreadsheet_name}` detected.")
+
+    current_spreadsheet = (
+        sheets_service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    )
+    current_sheets = current_spreadsheet.get("sheets", [])
+    current_sheet_title_set = set(
+        sheet["properties"]["title"] for sheet in current_sheets
+    )
+    requests = []
+    for sheet_title in sheet_titles:
+        if not sheet_title in current_sheet_title_set:
+            requests.append(
+                {
+                    "addSheet": {
+                        "properties": {
+                            "title": sheet_title,
+                        }
+                    }
+                }
+            )
+    batch_update_spreadsheet_response = (
+        sheets_service.spreadsheets()
+        .batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body={"requests": requests},
+        )
+        .execute()
+    )
+    return spreadsheet_id
 
 
 @router.get("/google", response_model=ResponseSchema[GetGoogleResponse])
@@ -70,13 +107,16 @@ async def patch_google(
     update_google: UpdateGoogleRequest,
     current_end_user: dict = Depends(get_current_end_user),
     drive_service: Resource = Depends(get_drive_v3_service),
+    sheets_service: Resource = Depends(get_sheets_v4_service),
     chore_master_api_db: MongoDB = Depends(get_chore_master_api_db),
 ):
     profile_folder_id = update_google.profile_folder_id
     treasury_spreadsheet_id = create_spreadsheet_if_not_exist(
         parent_folder_id=profile_folder_id,
         spreadsheet_name="treasury",
+        sheet_titles=["account"],
         drive_service=drive_service,
+        sheets_service=sheets_service,
     )
     end_user_collection = chore_master_api_db.get_collection("end_user")
     end_user_collection.update_one(
