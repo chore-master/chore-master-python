258a7ae311c02c7d35bb41ecba4715d29dc31bc7
CP Weng <cp.weng@tkspring.com>
2024-06-03 17:56:44 +0800

    Update

--- chore_master_api/models/entity.py
+++ chore_master_api/models/entity.py
@@ -1,2 +1,7 @@
-class Entity:
-    reference: str
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field
+
+
+class Entity(BaseModel):
+    reference: UUID = Field(default_factory=uuid4)
