04d974bfea903066cfd5f287d46172218af4fa0e
gocreating <gocreating@gmail.com>
2024-07-06 16:30:21 +0800

    Update

--- apps/bill_processor/entrypoints/report_portfolio_plot_from_bill.py
+++ apps/bill_processor/entrypoints/report_portfolio_plot_from_bill.py
@@ -1,31 +1,67 @@
 import asyncio
 import os
+from contextlib import contextmanager
 
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.dates import DateFormatter
+from matplotlib.ticker import FuncFormatter
+
+
+@contextmanager
+def portfolio_plot():
+    def comma_formatter(x, pos):
+        return f"{x:,.0f}"
+
+    custom_color_cycle = [
+        "#1f77b4",
+        "#ff7f0e",
+        "#2ca02c",
+        "#d62728",
+        "#9467bd",
+        "#8c564b",
+        "#e377c2",
+        "#7f7f7f",
+        "#bcbd22",
+        "#17becf",
+    ]
+    plt.rcParams["axes.prop_cycle"] = plt.cycler(color=custom_color_cycle)
+    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True)
+    axs[-1].tick_params(axis="x", rotation=30)
+    ax_settlement, ax_inventory = axs
+    ax_inventory.xaxis.set_major_formatter(DateFormatter("%Y-%m"))
+    ax_inventory.yaxis.get_major_formatter().set_scientific(False)
+    ax_settlement.yaxis.get_major_formatter().set_scientific(False)
+    ax_inventory.yaxis.set_major_formatter(FuncFormatter(comma_formatter))
+    ax_settlement.yaxis.set_major_formatter(FuncFormatter(comma_formatter))
+    ax_settlement.set_ylabel("Settlement Amount")
+    ax_settlement.axhline(0, color="gray", linestyle="--")
+    ax_inventory.set_ylabel("Inventory Amount")
+    ax_inventory.grid(True)
+    yield fig, axs
+    plt.close()
 
 
 async def plot_equity_curve(symbol: str, symbol_df: pd.DataFrame, ax: plt.Axes):
-    # symbol_df["_utc_time"] = pd.to_datetime(symbol_df["utc_time"])
     grouped_by_time = symbol_df.groupby(["_utc_time"])
     aggregated_df = (grouped_by_time["amount_change"].sum().reset_index()).sort_values(
         by=["_utc_time"], ascending=[True]
     )
     aggregated_df["equity_amount"] = aggregated_df["amount_change"].cumsum()
-    # fig, ax = plt.subplots()
-    # plt.xticks(rotation=30)
-    ax.bar(aggregated_df["_utc_time"], aggregated_df["amount_change"], label=symbol)
-    ax.plot(aggregated_df["_utc_time"], aggregated_df["equity_amount"], label=symbol)
-    # ax.set_xlabel("Time")
-    # ax.xaxis.set_major_formatter(DateFormatter("%Y-%m"))
-    # ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:,.0f}"))
-    # ax.yaxis.get_major_formatter().set_scientific(False)
-    # ax.set_ylabel("Amount")
-    # ax.grid(True)
-    # ax.set_title(f"Equity Curve for {symbol}")
-    # fig.savefig(f"apps/bill_processor/build/{symbol}.png", bbox_inches="tight")
-    # plt.close()
+    ax.bar(
+        aggregated_df["_utc_time"],
+        aggregated_df["amount_change"],
+        label=symbol,
+        width=1.5,
+    )
+    ax.plot(
+        aggregated_df["_utc_time"],
+        aggregated_df["equity_amount"],
+        label=symbol,
+        linewidth=0.8,
+        marker=".",
+        markersize=1,
+    )
 
 
 async def plot_portfolio(
@@ -34,19 +70,6 @@ async def plot_portfolio(
     session_reference_set: set[str],
     bill_df: pd.DataFrame,
 ):
-    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True)
-    fig.suptitle(f"{portofolio_name}")
-    axs[-1].tick_params(axis="x", rotation=30)
-    ax_settlement, ax_inventory = axs
-    ax_inventory.xaxis.set_major_formatter(DateFormatter("%Y-%m"))
-    # ax_inventory.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:,.0f}"))
-    ax_inventory.yaxis.get_major_formatter().set_scientific(False)
-    ax_settlement.yaxis.get_major_formatter().set_scientific(False)
-    ax_settlement.set_ylabel("Settlement Amount")
-    ax_settlement.axhline(0, color="gray", linestyle="--")
-    ax_inventory.set_ylabel("Inventory Amount")
-    ax_inventory.grid(True)
-
     portofolio_bill_df = bill_df[
         bill_df["session_reference"].isin(session_reference_set)
     ]
@@ -56,19 +79,21 @@ async def plot_portfolio(
     inventory_bill_df = portofolio_bill_df[
         portofolio_bill_df["symbol"] != settlement_symbol
     ]
-    await plot_equity_curve(settlement_symbol, settlement_bill_df, ax_settlement)
 
-    grouped_by_symbol = inventory_bill_df.groupby(["symbol"])
-    for (symbol,), symbol_df in grouped_by_symbol:
-        await plot_equity_curve(symbol, symbol_df, ax_inventory)
+    with portfolio_plot() as (fig, (ax_settlement, ax_inventory)):
+        fig.suptitle(f"{portofolio_name}")
 
-    ax_settlement.legend(loc="upper left", bbox_to_anchor=(1.04, 1))
-    ax_inventory.legend(loc="upper left", bbox_to_anchor=(1.04, 1))
-    fig.savefig(
-        f"apps/bill_processor/build/portofolio_{portofolio_name}.png",
-        bbox_inches="tight",
-    )
-    plt.close()
+        await plot_equity_curve(settlement_symbol, settlement_bill_df, ax_settlement)
+        grouped_by_symbol = inventory_bill_df.groupby(["symbol"])
+        for (symbol,), symbol_df in grouped_by_symbol:
+            await plot_equity_curve(symbol, symbol_df, ax_inventory)
+
+        ax_settlement.legend(loc="upper left", bbox_to_anchor=(1.04, 1))
+        ax_inventory.legend(loc="upper left", bbox_to_anchor=(1.04, 1))
+        fig.savefig(
+            f"apps/bill_processor/build/portofolio_{portofolio_name}.png",
+            bbox_inches="tight",
+        )
 
 
 async def plot_portfolios(bill_df: pd.DataFrame):
