544c775270fde244da5de4e5b1ff10c82781f497
gocreating <gocreating@gmail.com>
2024-09-01 15:45:35 +0800

    Update

--- apps/chore_master_api/web_server/dependencies/unit_of_work.py
+++ apps/chore_master_api/web_server/dependencies/unit_of_work.py
@@ -1,11 +1,20 @@
 from typing import Type
 
 from fastapi import Depends
+from sqlalchemy.orm import registry
 
+from apps.chore_master_api.unit_of_works.some_module_unit_of_work import (
+    SomeModuleSQLAlchemyUnitOfWork,
+)
 from apps.chore_master_api.web_server.dependencies.auth import get_current_end_user
+from apps.chore_master_api.web_server.dependencies.end_user_database import (
+    get_end_user_db,
+    get_end_user_db_registry,
+)
 from apps.chore_master_api.web_server.dependencies.google_service import (
     get_google_service,
 )
+from modules.database.relational_database import RelationalDatabase
 from modules.google_service.google_service import GoogleService
 from modules.unit_of_works.base_spreadsheet_unit_of_work import (
     BaseSpreadsheetUnitOfWork,
@@ -33,3 +42,10 @@ def get_spreadsheet_unit_of_work_factory(
         )
 
     return _get_spreadsheet_unit_of_work
+
+
+async def get_some_module_uow(
+    end_user_db: RelationalDatabase = Depends(get_end_user_db),
+    _end_user_db_registry: registry = Depends(get_end_user_db_registry),
+) -> SomeModuleSQLAlchemyUnitOfWork:
+    return SomeModuleSQLAlchemyUnitOfWork(relational_database=end_user_db)
