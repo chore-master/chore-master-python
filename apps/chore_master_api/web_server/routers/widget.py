2bc2bc09603cd597becd6a95f3ddb88021adede0
CP Weng <cp.weng@tkspring.com>
2025-01-21 13:00:02 +0800

    Update

--- apps/chore_master_api/web_server/routers/widget.py
+++ apps/chore_master_api/web_server/routers/widget.py
@@ -5,6 +5,305 @@ from modules.web_server.schemas.response import ResponseSchema, StatusEnum
 router = APIRouter(prefix="/widget", tags=["Widget"])
 
 
+@router.get("/web3-ecosystem")
+async def get_web3_ecosystem():
+    color_map = {
+        "person": "blue",
+        "organiation": "red",
+        "blockchain": "grey",
+        "project": "green",
+        "token": "yellow",
+    }
+
+    blockchains = [
+        {
+            "id": "chain_ethereum",
+            "name": "Ethereum",
+            "to_token": ["token_eth"],
+        },
+        {
+            "id": "chain_solana",
+            "name": "Solana",
+            "to_token": ["token_sol"],
+        },
+        {
+            "id": "chain_tron",
+            "name": "Tron",
+        },
+        {
+            "id": "chain_bsc",
+            "name": "BSC",
+        },
+        {
+            "id": "chain_base",
+            "name": "Base",
+            "from_person": ["person_jesse_pollak"],
+        },
+        {
+            "id": "chain_bitcoin",
+            "name": "Bitcoin",
+            "to_token": ["token_btc"],
+        },
+        {
+            "id": "chain_arbitrum",
+            "name": "Arbitrum",
+        },
+        {
+            "id": "chain_avalanche",
+            "name": "Avalanche",
+        },
+        {
+            "id": "chain_sui",
+            "name": "Sui",
+        },
+        {
+            "id": "chain_hyperliquid",
+            "name": "Hyperliquid",
+        },
+        {
+            "id": "chain_aptos",
+            "name": "Aptos",
+        },
+    ]
+
+    people = [
+        {
+            "id": "person_shaw",
+            "name": "Shaw",
+        },
+        {
+            "id": "person_jesse_pollak",
+            "name": "Jesse Pollak",
+            "avatar_url": "https://s3.amazonaws.com/finn--production/uploads/public_8f3e472e-bac5-4913-bfc4-f80ef8d27ceb_image.jpeg",
+        },
+        {
+            "id": "person_michael_egorov",
+            "name": "Michael Egorov",
+            "avatar_url": "https://media.licdn.com/dms/image/v2/C5603AQEqg8W79nphvA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1516508038501?e=2147483647&v=beta&t=ge60gZwH8Pd3b5IDVQhLHTnZzeq_1qblKJER1Ye3N5Y",
+        },
+        {
+            "id": "person_jerry_li",
+            "name": "Jerry Li",
+        },
+        {
+            "id": "person_corey_caplan",
+            "name": "Corey Caplan",
+            "avatar_url": "https://www.shirleywachtel.com/wp-content/uploads/2023/02/Corey-Caplan.jpg",
+        },
+        {
+            "id": "person_guy_young",
+            "name": "Guy Young",
+            "avatar_url": "https://cdn.prod.website-files.com/645edd8f8709e3387ae97c68/663b7d7ab6281c3238157f72_2099_Guy-Young%20(1).jpeg",
+        },
+        {
+            "id": "person_pierre_person",
+            "name": "Pierre PERSON",
+            "avatar_url": "https://pbs.twimg.com/profile_images/1792630849612976129/8Qdeh-Yb_400x400.jpg",
+        },
+    ]
+
+    organiations = [
+        {
+            "id": "org_iosg",
+            "name": "IOSG Ventures",
+        },
+        {
+            "id": "org_eliza_labs",
+            "name": "Eliza Labs",
+        },
+        {
+            "id": "org_ai16z",
+            "name": "ai16z DAO",
+            "from_org": ["org_eliza_labs"],
+            "from_person": ["person_shaw"],
+        },
+        {
+            "id": "org_term_structure_labs",
+            "name": "Term Structure Labs",
+            "from_person": ["person_jerry_li"],
+        },
+        {
+            "id": "org_usual_money",
+            "name": "Usual Money",
+            "from_person": ["person_pierre_person"],
+            "to_token": ["token_usual"],
+        },
+        {
+            "id": "org_ethena_labs",
+            "name": "Ethena Labs",
+            "from_person": ["person_guy_young"],
+            "to_token": ["token_ena"],
+        },
+        {
+            "id": "org_leavitt_innovations",
+            "name": "Leavitt Innovations",
+            "from_person": ["person_corey_caplan"],
+        },
+    ]
+
+    apps = [
+        {
+            "id": "app_termstructure",
+            "name": "Term Structure",
+            "from_org": ["org_term_structure_labs"],
+            "to_chain": ["chain_ethereum", "chain_arbitrum"],
+        },
+        {
+            "id": "app_termmax",
+            "name": "TermMax",
+            "from_org": ["org_term_structure_labs"],
+            "to_chain": ["chain_ethereum", "chain_arbitrum"],
+        },
+        {
+            "id": "app_ethena",
+            "name": "Ethena",
+            "from_org": ["org_ethena_labs"],
+            "to_chain": ["chain_ethereum"],
+        },
+        {
+            "id": "app_usual",
+            "name": "Usual",
+            "from_org": ["org_usual_money"],
+            "to_chain": ["chain_ethereum", "chain_arbitrum"],
+        },
+        {
+            "id": "app_dolomite",
+            "name": "Dolomite",
+            "from_org": ["org_leavitt_innovations"],
+            "to_chain": ["chain_arbitrum"],
+        },
+        {
+            "id": "app_pump_fun",
+            "name": "Pump.fun",
+            "to_chain": ["chain_solana"],
+        },
+    ]
+
+    tokens = [
+        {
+            "id": "token_btc",
+            "name": "BTC",
+            "full_name": "Bitcoin",
+        },
+        {
+            "id": "token_eth",
+            "name": "ETH",
+            "full_name": "Ethereum",
+        },
+        {
+            "id": "token_sol",
+            "name": "SOL",
+            "full_name": "Solana",
+        },
+        {
+            "id": "token_bayc",
+            "name": "BAYC",
+            "full_name": "Bored Ape Yacht Club",
+        },
+        {
+            "id": "token_usual",
+            "name": "USUAL",
+        },
+        {
+            "id": "token_usd0",
+            "name": "USD0",
+            "from_app": ["app_usual"],
+        },
+        {
+            "id": "token_usd0++",
+            "name": "USD0++",
+            "from_token": ["token_usd0"],
+        },
+        {
+            "id": "token_ena",
+            "name": "ENA",
+        },
+        {
+            "id": "token_usde",
+            "name": "USDe",
+            "from_app": ["app_ethena"],
+        },
+        {
+            "id": "token_susde",
+            "name": "sUSDe",
+            "from_token": ["token_usde"],
+        },
+        {
+            "id": "token_dolo",
+            "name": "DOLO",
+            "from_app": ["app_dolomite"],
+        },
+        {
+            "id": "token_odolo",
+            "name": "oDOLO",
+            "from_token": ["token_dolo"],
+        },
+        {
+            "id": "token_vedolo",
+            "name": "veDOLO",
+            "from_token": ["token_odolo"],
+        },
+    ]
+
+    links = []
+    entities = [*people, *organiations, *blockchains, *apps, *tokens]
+    dimensions = ["person", "org", "chain", "app", "token"]
+    for dimension in dimensions:
+        for entity in entities:
+            key = f"from_{dimension}"
+            if key in entity:
+                for fromEntityId in entity[key]:
+                    links.append([fromEntityId, entity["id"]])
+            key = f"to_{dimension}"
+            if key in entity:
+                for toEntityId in entity[key]:
+                    links.append([entity["id"], toEntityId])
+    nodes = [
+        *[
+            {
+                **item,
+                "color": color_map["person"],
+            }
+            for item in people
+        ],
+        *[
+            {
+                **item,
+                "color": color_map["organiation"],
+            }
+            for item in organiations
+        ],
+        *[
+            {
+                **item,
+                "color": color_map["blockchain"],
+            }
+            for item in blockchains
+        ],
+        *[
+            {
+                **item,
+                "color": color_map["project"],
+            }
+            for item in apps
+        ],
+        *[
+            {
+                **item,
+                "color": color_map["token"],
+            }
+            for item in tokens
+        ],
+    ]
+    return ResponseSchema[dict](
+        status=StatusEnum.SUCCESS,
+        data={
+            "nodes": nodes,
+            "links": links,
+        },
+    )
+
+
 @router.get("/sankey")
 async def get_sankey():
     return ResponseSchema[dict](
