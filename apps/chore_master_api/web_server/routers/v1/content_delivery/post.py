10f7a4e767f81b7c63b0cdc37d1a30fbba5d4618
gocreating <gocreating@gmail.com>
2025-03-18 23:39:44 +0800

    Update

--- apps/chore_master_api/web_server/routers/v1/content_delivery/post.py
+++ apps/chore_master_api/web_server/routers/v1/content_delivery/post.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Header, Path
 
 from apps.chore_master_api.web_server.schemas.response import BaseQueryEntityResponse
 from modules.web_server.exceptions import BadRequestError
@@ -8,19 +8,24 @@ from modules.web_server.schemas.response import ResponseSchema, StatusEnum
 
 router = APIRouter()
 
+PROJECT_API_KEY = "DfuhFarHVvRKPt2y7doapje5"
+
 posts = [
     {
         "reference": "post-1",
+        "project_api_key": PROJECT_API_KEY,
         "title": "Post 1",
         "content": "Content 1",
     },
     {
         "reference": "post-2",
+        "project_api_key": PROJECT_API_KEY,
         "title": "Post 2",
         "content": "Content 2",
     },
     {
         "reference": "post-3",
+        "project_api_key": PROJECT_API_KEY,
         "title": "Post 3",
         "content": """
 # Test
@@ -48,7 +53,7 @@ class ReadPostDetailResponse(BaseQueryEntityResponse):
 
 
 @router.get("/posts")
-async def get_posts():
+async def get_posts(x_project_api_key: Annotated[str | None, Header()] = None):
     return ResponseSchema[list[ReadPostSummaryResponse]](
         status=StatusEnum.SUCCESS,
         data=[
@@ -57,6 +62,7 @@ async def get_posts():
                 title=post["title"],
             )
             for post in posts
+            if post["project_api_key"] == x_project_api_key
         ],
     )
 
@@ -64,8 +70,17 @@ async def get_posts():
 @router.get("/posts/{post_reference}")
 async def get_posts_post_reference(
     post_reference: Annotated[str, Path()],
+    x_project_api_key: Annotated[str | None, Header()] = None,
 ):
-    post = next((post for post in posts if post["reference"] == post_reference), None)
+    post = next(
+        (
+            post
+            for post in posts
+            if post["reference"] == post_reference
+            and post["project_api_key"] == x_project_api_key
+        ),
+        None,
+    )
     if not post:
         raise BadRequestError("Post not found")
     return ResponseSchema[ReadPostDetailResponse](status=StatusEnum.SUCCESS, data=post)
